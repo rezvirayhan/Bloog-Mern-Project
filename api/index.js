0aeb7facb308ca166bebda3e88279c0f69ec470b
rezvirayhan <devrezvi.rayhan@gmail.com>
2024-01-30 15:55:52 +0600

    add funcon to the signup pagess

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
@@ -13,6 +14,7 @@ mongoose.connect(process.env.MONGO)
     });
 
 const app = express();
+app.use(cors())
 app.use(express.json())
 
 app.listen(3000, () => {
--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,7 +1,110 @@
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 const SignUp = () => {
+    const [formData, setFormData] = useState({})
+    const [errorMassage, setErrorMassage] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value.trim() })
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!formData.username || !formData.email || !formData.password) {
+            return setErrorMassage("Please Fill out all fildes")
+        }
+        try {
+            setLoading(true)
+            setErrorMassage(null)
+            const res = await fetch('/api/auth/signup', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+            if (data.success === false) {
+                return setErrorMassage(data.message)
+            }
+            setLoading(false)
+            if (res.ok) {
+                navigate('/sign-in')
+            }
+        } catch (error) {
+            setErrorMassage(error.message)
+            setLoading(false)
+        }
+    };
     return (
-        <div>
-            <h2>Sign-Up Pages</h2>
+        <div className="min-h-screen mt-20 ">
+            <div className="flex p-4 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+                {/* left side */}
+                <div className="flex-1">
+                    <Link to="/" className="font-bold dark:text-white text-4xl" >
+                        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-orange-500 rounded-lg text-white">Rezvi's</span>
+                        Blog
+                    </Link>
+                    <p className="text-sm mt-5">
+                        This is a demo Project. You can Sign Up with your email and password Or with Google
+                    </p>
+                </div>
+                {/* right side */}
+
+                <div className="flex-1">
+                    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                        <div>
+                            <Label value="Your username" />
+                            <TextInput
+                                type="text"
+                                placeholder="UserName"
+                                id="username"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div>
+                            <Label value="Your Email" />
+                            <TextInput
+                                type="email"
+                                placeholder="Your Email"
+                                id="email"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div>
+                            <Label value="Your Password" />
+                            <TextInput
+                                type="password"
+                                placeholder="Your Password"
+                                id="password"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <Button gradientDuoTone="purpleToPink" type="submit" disabled={loading}>
+                            {
+                                loading ? (
+                                    <>
+                                        <Spinner size='sm' />
+                                        <span className="pl-3">Loading...</span>
+                                    </>
+
+                                ) : 'Sign-Up'
+                            }
+                        </Button>
+                    </form>
+                    <div className="flex gap-2 text-sm mt-5">
+                        <span>Have an account</span>
+                        <Link className="text-blue-600 font-semibold" to="/sign-in">Sign-In</Link>
+                    </div>
+                    {
+                        errorMassage && (
+                            <Alert className="mt-5" color="failure">
+                                {errorMassage}
+                            </Alert>
+                        )
+                    }
+                </div>
+            </div>
         </div>
     );
 };
