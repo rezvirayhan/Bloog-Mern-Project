920e7c3fa364f5dd44ae1e6bfe54a766cc0ac4fa
rezvirayhan <devrezvi.rayhan@gmail.com>
2024-02-01 16:48:32 +0600

    added redux tooklit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -44,10 +44,7 @@ export const signin = async (req, res, next) => {
         const token = jwt.sign(
             { id: validUser._id }, process.env.JWT_SECRET
         );
-
-
         const { password: pass, ...rest } = validUser._doc;
-
         res
             .status(200)
             .cookie('access_token', token, {
