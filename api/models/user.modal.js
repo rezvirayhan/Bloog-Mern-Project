74abf4cc429b659da20e4fc08bd6951337e6dc71
rezvirayhan <devrezvi.rayhan@gmail.com>
2024-02-01 19:17:45 +0600

    added OAuth Fucntion Done

--- api/models/user.modal.js
+++ api/models/user.modal.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        profilePicture: {
+            type: String,
+            default: "https://static.vecteezy.com/system/resources/previews/003/429/299/non_2x/male-silhouette-neon-light-icon-gentlemen-wc-door-glowing-sign-vector.jpg"
+        }
     },
     { timestamps: true }
 );
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,16 +1,33 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { Button } from "flowbite-react";
 import { AiFillGoogleCircle } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { app } from "../firebase";
-
+import { signInSuccess } from "../redux/user/userSlice";
 const OAuth = () => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
     const handleGoogleClick = async () => {
         const auth = getAuth(app)
         const provider = new GoogleAuthProvider()
         provider.setCustomParameters({ prompt: "select_account" })
         try {
             const resultFormGoogle = await signInWithPopup(auth, provider)
-            // console.log(resultFormGoogle);   
+            const res = await fetch('/api/auth/google', {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    name: resultFormGoogle.user.displayName,
+                    email: resultFormGoogle.user.email,
+                    googlePhotoUrl: resultFormGoogle.user.photoURL
+                }),
+            })
+            const data = await res.json()
+            if (res.ok) {
+                dispatch(signInSuccess(data))
+                navigate('/')
+            }
         } catch (error) {
             console.log(error);
         }
