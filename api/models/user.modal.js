27a8ca9c8f8f1391501655d452d806fce8895859
rezvirayhan <devrezvi.rayhan@gmail.com>
2024-02-05 18:25:44 +0600

    add admin function

--- api/models/user.modal.js
+++ api/models/user.modal.js
@@ -19,7 +19,11 @@ const userSchema = new mongoose.Schema(
         profilePicture: {
             type: String,
             default: "https://static.vecteezy.com/system/resources/previews/003/429/299/non_2x/male-silhouette-neon-light-icon-gentlemen-wc-door-glowing-sign-vector.jpg"
-        }
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false
+        },
     },
     { timestamps: true }
 );
