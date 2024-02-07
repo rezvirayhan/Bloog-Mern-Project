fc10b9b13bdc171526ccef590fde7f93f1fc8c5f
rezvirayhan <devrezvi.rayhan@gmail.com>
2024-02-07 16:48:42 +0600

    complite show more button done

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,8 +1,124 @@
+import { Table } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const DashPosts = () => {
+    const { currentUser } = useSelector((state) => state.user);
+    const [userPosts, setUserPosts] = useState([]);
+    const [showMore, setShowMore] = useState(true);
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+                const data = await res.json();
+                if (res.ok) {
+                    setUserPosts(data.posts);
+                    if (data.posts.length < 9) {
+                        setShowMore(false);
+                    }
+                }
+            } catch (error) {
+                console.log(error.message);
+            }
+        };
+        if (currentUser.isAdmin) {
+            fetchPosts();
+        }
+    }, [currentUser._id]);
+
+    const handleShowMore = async () => {
+        const startIndex = userPosts.length;
+        try {
+            const res = await fetch(
+                `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+            );
+            const data = await res.json();
+            if (res.ok) {
+                setUserPosts((prev) => [...prev, ...data.posts]);
+                if (data.posts.length < 9) {
+                    setShowMore(false);
+                }
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+
+
+
     return (
-        <div>
-            <h2>hello fuck</h2>
+        <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+            {currentUser.isAdmin && userPosts.length > 0 ? (
+                <>
+                    <Table hoverable className='shadow-md'>
+                        <Table.Head>
+                            <Table.HeadCell>Date updated</Table.HeadCell>
+                            <Table.HeadCell>Post image</Table.HeadCell>
+                            <Table.HeadCell>Post title</Table.HeadCell>
+                            <Table.HeadCell>Category</Table.HeadCell>
+                            <Table.HeadCell>Delete</Table.HeadCell>
+                            <Table.HeadCell>
+                                <span>Edit</span>
+                            </Table.HeadCell>
+                        </Table.Head>
+                        {userPosts.map((post) => (
+                            <Table.Body key={post._id} className='divide-y'>
+                                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                                    <Table.Cell>
+                                        {new Date(post.updatedAt).toLocaleDateString()}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        <Link to={`/post/${post.slug}`}>
+                                            <img
+                                                src={post.image}
+                                                alt={post.title}
+                                                className='w-20 h-10 object-cover bg-gray-500'
+                                            />
+                                        </Link>
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        <Link
+                                            className='font-medium text-gray-900 dark:text-white'
+                                            to={`/post/${post.slug}`}
+                                        >
+                                            {post.title}
+                                        </Link>
+                                    </Table.Cell>
+                                    <Table.Cell>{post.category}</Table.Cell>
+                                    <Table.Cell>
+                                        <span
+                                            className='font-medium text-red-500 hover:underline cursor-pointer'
+                                        >
+                                            Delete
+                                        </span>
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        <Link
+                                            className='text-teal-500 hover:underline'
+                                            to={`/update-post/${post._id}`}
+                                        >
+                                            <span>Edit</span>
+                                        </Link>
+                                    </Table.Cell>
+                                </Table.Row>
+                            </Table.Body>
+                        ))}
+                    </Table>
+                    {showMore && (
+                        <button
+                            onClick={handleShowMore}
+                            className=' w-full  mt-5 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'
+                        >
+                            Show More
+                        </button>
+                    )}
+                </>
+            ) : (
+                <p>You have no posts yet!</p>
+            )}
+
         </div>
     );
 };
